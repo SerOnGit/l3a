4104740b01b1e6aead1afdcfeea9830e179a10b5
SerOnGit <sgegi87@gmail.com>
2022-12-20 02:24:58 +0400

    publish

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
-
-
 set :application, "l3"
-set :repo_url, "git@github.com:username/l3.git"
+set :repo_url, "git@github.com:SerOnGit/l3.git"
 
 # Deploy to the user's home directory
 set :deploy_to, "/home/deploy/#{fetch :application}"
